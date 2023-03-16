28c1ff2166f67a8ec3b307aa31add6551f078dd2
Vovikkko <45092587+Beenlord@users.noreply.github.com>
2023-03-17 00:58:15 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore sapiente nesciunt mollitia natus ea illum eum enim sequi, a doloribus. Minima obcaecati consequuntur corporis fugiat itaque repellendus ea omnis quas.
-    </p>
+    <h1>Привет, Саня!</h1>
   </div>
 </template>
 
