17d0c9285eaa84679bf697559d7d1f913d3ae06b
Vovikkko <45092587+Beenlord@users.noreply.github.com>
2023-03-17 00:48:07 +0300

    test commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <Tutorial />
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore sapiente nesciunt mollitia natus ea illum eum enim sequi, a doloribus. Minima obcaecati consequuntur corporis fugiat itaque repellendus ea omnis quas.
+    </p>
+  </div>
 </template>
 
 <script>
